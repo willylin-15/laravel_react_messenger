f4539ab7433a5ac1778112cb2ef77a52abfe6ead
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-26 17:19:41 +0630

    Add 2 fillables to conversation model

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Conversation extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id1',
+        'user_id2'
+    ];
 }
