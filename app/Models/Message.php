e42483a05e6cce567bd1221dfa4a3f73ac8bfc5c
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-26 17:16:55 +0630

    Add 4 fillables to message model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'message',
+        'sender_id',
+        'group_id',
+        'receiver_id'
+    ];
 }
