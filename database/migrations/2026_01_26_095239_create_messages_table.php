a3c977120df991bf4244c3fefd73552ce78b486e
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-26 16:54:51 +0630

    Add 4 foreignId cols to messages table

--- database/migrations/2026_01_26_095239_create_messages_table.php
+++ database/migrations/2026_01_26_095239_create_messages_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->longText('message')->nullable();
+            $table->foreignId('sender_id')->constrained('users');
+            $table->foreignId('receiver_id')->nullable()->constrained('users');
+            $table->foreignId('group_id')->nullable()->constrained('groups');
+            $table->foreignId('conversation_id')->nullable()->constrained('conversations');
             $table->timestamps();
         });
     }
