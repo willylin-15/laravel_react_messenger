e348ced01bcc53827e27ef3de0feb9b557398450
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-26 17:51:15 +0630

    Add 2 fakers in GroupFactory

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -17,7 +17,8 @@ class GroupFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'description' => $this->faker->sentence,
         ];
     }
 }
