4217a19b213e0fa116409910a139b491ace1583a
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-26 17:18:19 +0630

    Add 5 fillables to messageattachment model

--- app/Models/MessageAttachment.php
+++ app/Models/MessageAttachment.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MessageAttachment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'message_id',
+        'name',
+        'path',
+        'mime',
+        'size'
+    ];
 }
