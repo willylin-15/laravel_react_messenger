65f9f09932f6d9473dc4f53013f766e160675602
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-26 16:46:28 +0630

    Create group_users table with 4 cols

--- database/migrations/2026_01_26_094918_create_groups_table.php
+++ database/migrations/2026_01_26_094918_create_groups_table.php
@@ -18,6 +18,13 @@ return new class extends Migration
             $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
+
+        Schema::create('group_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('group_id')->constrained('groups')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
     }
 
     /**
