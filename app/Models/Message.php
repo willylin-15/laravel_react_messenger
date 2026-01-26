4db6033ed09012e6531b0614a61e3bf6654bd1f5
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-26 17:42:43 +0630

    Add 3 funcs to Message Model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -15,4 +15,21 @@ class Message extends Model
         'group_id',
         'receiver_id'
     ];
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver() {
+        return $this->belongsTo(User::class,'receiver_id');
+    }
+
+    public function group() {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function attachments() {
+        return $this->hasMany(MessageAttachment::class);
+    }
 }
