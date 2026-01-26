470f2454b2c9f689ba7568cf155ecbb5dd5df7fd
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-26 17:45:17 +0630

    Add 3 relation funcs to Conversation Model

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -14,4 +14,17 @@ class Conversation extends Model
         'user_id2',
         'last_message_id'
     ];
+
+    public function lastMessage()
+    {
+        return $this->belongsTo(Message::class, 'last_message_id');
+    }
+
+    public function user1() {
+        return $this->belongsTo(User::class,'user_id1');
+    }
+
+    public function user2() {
+        return $this->belongsTo(User::class,'user_id2');
+    }
 }
