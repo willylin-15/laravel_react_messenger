f881ca2c4c09777d0cade0ead5ba44be464cc96f
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-27 11:36:59 +0630

    Renaem Home function

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
-export default function Dashboard() {
+export default function Home() {
     return (
         <AuthenticatedLayout
             header={
