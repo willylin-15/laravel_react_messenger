bd1627db25fca1e076c5972ecba6d8cbd78b5440
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-27 14:27:58 +0630

    Remove useEffect library

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -3,7 +3,7 @@ import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link, usePage } from '@inertiajs/react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function AuthenticatedLayout({ header, children }) {
     const page = usePage();
