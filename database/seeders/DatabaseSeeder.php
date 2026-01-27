c8311ea60b9b1b40796db3f174e3528db4866c1c
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-27 10:23:13 +0630

    Generate one admin and user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'John Doe',
+            'email'=> 'john@example.com',
+            'password'=> bcrypt('password'),
+            'is_admin'=> true,
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name'=> 'Jane Doe',
+            'email'=> 'jane@example.com',
+            'password'=> bcrypt('password'),
         ]);
     }
 }
