114772ca20c09289aac89a3e97cec5a8830c799e
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-27 15:13:51 +0630

    Define UserResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class UserResource extends JsonResource
 {
@@ -14,6 +15,16 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'avatar_url' => $this->avatar ? Storage::url($this->avatar) : null,
+            'name'=> $this->name,
+            'email' => $this->email,
+            'created_at' => $this->created_at,
+            'updated_at'=> $this->updated_at,
+            'is_admin' => (bool) $this->is_admin,
+            'last_message' => $this->last_message,
+            'last_message_date' => $this->last_message_date,
+        ];
     }
 }
