048eae48fbc88998a0b67e2c2ac22dcf227a56c0
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-27 14:01:38 +0630

    Undo Edit.jsx

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -4,9 +4,16 @@ import DeleteUserForm from './Partials/DeleteUserForm';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
 
-function Edit({ auth, mustVerifyEmail, status }) {
+export default function Edit({ auth, mustVerifyEmail, status }) {
     return (
-        <>
+        <AuthenticatedLayout
+            user={auth.user}
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                    Profile
+                </h2>
+            }
+        >
             <Head title="Profile" />
 
             <div className="py-12">
@@ -28,18 +35,6 @@ function Edit({ auth, mustVerifyEmail, status }) {
                     </div>
                 </div>
             </div>
-        </>
+        </AuthenticatedLayout>
     );
 }
-
-Edit.layout = (page) => {
-    return (
-        <AuthenticatedLayout
-            user={page.props.auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Profile</h2>}
-            children={page}
-        ></AuthenticatedLayout>
-    );
-}
-
-export default Edit;
