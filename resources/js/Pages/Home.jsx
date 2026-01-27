4a0f41e8302eb59a14f9a9e141406fb245019a45
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-27 13:18:29 +0630

    Remove head

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,18 +1,8 @@
 import ChatLayout from '@/Layouts/ChatLayout';
-import { Head } from '@inertiajs/react';
 
 export default function Home({ auth }) {
     return (
-        <ChatLayout
-            user={auth.user}
-            header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                    Dashboard
-                </h2>
-            }
-        >
-            <Head title="Dashboard" />
-
+        <ChatLayout>
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
