076a311d4db6e97044a854fb020cab347cafe341
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-30 10:55:59 +0630

    Remove for loop

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,31 +34,33 @@ class DatabaseSeeder extends Seeder
 
         User::factory(10)->create();
 
-        for ($i = 0; $i < 5; $i++) {
-            $group = Group::factory()->create([
-                'owner_id'=> 1,
-            ]);
+        $group = Group::factory()->create([
+            'owner_id'=> 1,
+        ]);
 
-            $users = User::inRandomOrder()->limit(rand(2, 5))->pluck('id');
-            $group->users()->attach(array_unique([1, ...$users]));
+        $users = User::inRandomOrder()->limit(rand(2, 5))->pluck('id');
+        $group->users()->attach(array_unique([1, ...$users]));
 
-            Message::factory(1000)->create();
-            $messages = Message::whereNull('group_id')->orderBy('created_at')->get();
+        Message::factory(1000)->create();
+        $messages = Message::whereNull('group_id')->orderBy('created_at')->get();
 
-            $conversations = $messages->groupBy(function ($message) {
-                return collect([$message->sender_id, $message->receiver_id])->sort()->implode('_');
-            })->map(function ($groupedMessages) {
-                return [
-                    'user_id1' => $groupedMessages->first()->sender_id,
-                    'user_id2' => $groupedMessages->first()->receiver_id,
-                    'last_message_id' => $groupedMessages->last()->id,
-                    'created_at' => new Carbon(),
-                    'updated_at'=> new Carbon(),
-                ];
-            })->values();
+        $conversations = $messages->groupBy(function ($message) {
+            return collect([$message->sender_id, $message->receiver_id])->sort()->implode('_');
+        })->map(function ($groupedMessages) {
+            return [
+                'user_id1' => $groupedMessages->first()->sender_id,
+                'user_id2' => $groupedMessages->first()->receiver_id,
+                'last_message_id' => $groupedMessages->last()->id,
+                'created_at' => new Carbon(),
+                'updated_at'=> new Carbon(),
+            ];
+        })->values();
 
-            Conversation::insertOrIgnore($conversations->toArray());
+        Conversation::insertOrIgnore($conversations->toArray());
 
-        }
+        // for ($i = 0; $i < 5; $i++) {
+
+
+        // }
     }
 }
