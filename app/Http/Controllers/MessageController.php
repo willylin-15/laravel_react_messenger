ba2adf38ab251428d71953969f4eca9efb6c6bd8
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-01-30 11:37:38 +0630

    Add 5 functions

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreMessageRequest;
+use App\Models\Group;
+use App\Models\Message;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    public function byUser(User $user) {
+
+    }
+
+    public function byGroup(Group $group) {
+
+    }
+
+    public function loadOlder(Message $message) {
+
+    }
+
+    public function store(StoreMessageRequest $request) {
+
+    }
+
+    public function destroy(Message $message) {
+
+    }
 }
